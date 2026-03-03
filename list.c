6fb31fc9ccad1ef9cf59230db89b21397d1feafa
Jacob Todd <jacobt0722@gmail.com>
2026-03-03 15:34:12 -0800

    starting to create generic list data structure for the heap to make it more generic

--- list.c
+++ list.c
@@ -10,6 +10,15 @@ struct List {
 
 void initList(struct List *list, size_t stride, int total_size){
     list->array = malloc(stride * total_size);
+    list->stride = stride;
+    list->current_size = 0;
+    list->total_size = total_size;
+}
 
+void append(struct List *list, void *value){
+    if(list->current_size == list->total_size){
+        // Will replace return call with resize function
+        return;
+    }
 
 }
